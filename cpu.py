0eba29b867b75f0dc202064710ccc7995c144d9d
SolFox <Anburivas@gmail.com>
2017-12-05 01:01:29 -0500

    using psutil [WORK IN PROGRESS]

---
+++ cpu.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import psutil
+import os
+import curses
+import curses.textpad
+from curses.textpad import Textbox, rectangle
+
+def cpu():
+    for scpufreq in psutil.cpu_freq(percpu=True):
+        s = scpufreq.current 
+        print (int(s).__int__())
+           #int(s).__int__())
+#def cpux():
+#   x = psutil.cpu_count()
+
+#def cooling():
+
+    
+#def gpu():    
+    
+    
+    
+def mem():
+    print (os.popen('free -m -t | grep Mem').readlines())
+
+
+cpu()
+mem()
+#gpu()
+#cooling()
+#cpux()
