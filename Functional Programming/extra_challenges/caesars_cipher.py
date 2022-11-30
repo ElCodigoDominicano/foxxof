e63d12625f8fe3e11ae11bd1ed67bd26d3c1b8b4
El Codigo Dominicano <Anburivas@gmail.com>
2022-11-30 16:11:46 -0500

    Update caesars_cipher.py
    refactored code, added decoder.

--- Functional Programming/extra_challenges/caesars_cipher.py
+++ Functional Programming/extra_challenges/caesars_cipher.py
@@ -1,36 +1,37 @@
 """
-A module to encode with Caesar-style substitution ciphers.
+    A module to encode with Caesar-style substitution ciphers.
 
-Early (pre-20th century) encrypted messages letter substitution techniques.  To encrypt
-a message like this:
-    
-    Attack at dawn!
+    Early (pre-20th century) encrypted messages letter substitution techniques.  To encrypt
+    a message like this:
 
-you first removed all punctuation, spaces, and capitalization, like this:
-    
-    attackatdawn
-    
-The idea is that the reader can add all that information just inferring it from the text.
+        Attack at dawn!
 
-Now that the message only has letters, you then substitute each letter for another.  In 
-Caesar's original cipher, each letter was shifted 3 positions, wrapping around at the end.  
-So 'a' became 'd', 'b' become 'e',...,'w' became 'z', 'x' became 'a', 'y' became 'b' and 
-'z' became 'c'. So this message becomes 
-    
-    dwwdfndwgdzq
+    you first removed all punctuation, spaces, and capitalization, like this:
 
-To decrypt the message, you simply shift back in the reverse direction.  Or, you can just 
-shift 26-3 = 23 spots and you will wrap back around to the original message.  Because of
-this, rot-13 (shifting by 13 positions instead of 3) is another popular technique. 
-Encoding a message with rot-13 twice gives you the original message.
+        attackatdawn
 
-This way of encoding messages is no longer used for secure communication, because it
-is so easy to break.  But rot-13 can still be found on some Internet message boards
-where people use it to hide movie spoilers and for other light-hearted topics.
+    The idea is that the reader can add all that information just inferring it from the text.
 
-Author: AERivas
-Date: 06-04-2021
-Updated: 06-05-2022
+    Now that the message only has letters, you then substitute each letter for another.  In 
+    Caesar's original cipher, each letter was shifted 3 positions, wrapping around at the end.  
+    So 'a' became 'd', 'b' become 'e',...,'w' became 'z', 'x' became 'a', 'y' became 'b' and 
+    'z' became 'c'. So this message becomes 
+
+        dwwdfndwgdzq
+
+    To decrypt the message, you simply shift back in the reverse direction.  Or, you can just 
+    shift 26-3 = 23 spots and you will wrap back around to the original message.  Because of
+    this, rot-13 (shifting by 13 positions instead of 3) is another popular technique. 
+    Encoding a message with rot-13 twice gives you the original message.
+
+    This way of encoding messages is no longer used for secure communication, because it
+    is so easy to break.  But rot-13 can still be found on some Internet message boards
+    where people use it to hide movie spoilers and for other light-hearted topics.
+
+    Author: AERivas
+    Date: 06-04-2021
+    Updated: 06-05-2022
+    latest: 11-30-2022
 """
 def encode(text: str, n: int) -> str:
     """
@@ -56,15 +57,35 @@ def encode(text: str, n: int) -> str:
 
     result: str = ''
     alpha: str = 'abcdefghijklmnopqrstuvwxyz' * 2
-    for i in text:
-        if i in alpha:
-            # find the position of the info in [alpha] that correlates to
-            # the loop variable [i] in the entire loop [for i in text] up to [n];
-            # the shift positioner and pass it to the result.
-            result += alpha[alpha.index(i) + n]
+    for i in text.replace(" ",""):
+        if i.isalpha():
+             result += alpha[alpha.index(i) + n]
         else:
-            # Otherwise any info not in [text] and doesn't correlate to [i]
-            # will be passed to result.
-            result += i
+            result = "Nope."
     return result
 
+
+def decode(text: str, n: int):
+    """ Reversing the steps for the encode function """
+    assert isinstance(text, str), f'Parameter text must be a string not ->{repr(text)}.'
+    assert isinstance(n, int), f'Parameter n: must be an integer not -> {repr(n)}.'
+    assert 0 <= n < 26, 'This exceeds the maximum number allowed.'
+    
+    result: str = ""
+    alpha: str = "abcdefghijklmnopqrstuvwxyz" * 2
+    for i in text.replace(" ", ""):
+        if i.isalpha():
+            result += alpha[alpha.index(i) - n]
+        else:
+            result = "Nope."
+    return result
+
+if __name__ == '__main__':
+    first_message =  encode("land of the free",3)
+    second_message = encode("home of the brave",13)
+    
+    print(first_message)
+    print(decode(first_message,3))
+    
+    print(second_message)
+    print(decode(second_message,13))
