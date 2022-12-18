13d29dae77b28a8e0f03e7a12396c24d3b5f27b7
El Codigo Dominicano <Anburivas@gmail.com>
2022-12-18 09:03:57 -0500

    Code Refactoring
    conditionals, type hinting

--- password_generator.py
+++ password_generator.py
@@ -1,16 +1,5 @@
 """
- Random passphrase generator
- 
- when ran the user is given a choice from 1 of 5 options provided by the 
- generator and is provided with a length (8-65556) for the passphrase of their 
- choosing:
- 
- lower => lowercase alphabet (a-z)
- upper => uppercase alphabet(A-Z)
- digits => numerical (0-9)
- punct => punctuations (!@#$...)
- kitchen_sink => a mixture of all of the above 
- 
+ Random password generator
  using pythons built-in secrets and string 
  Author: AERivas
  Date: 12/12/2022
@@ -18,31 +7,9 @@
 import string
 import secrets
 
-def the_maker(choice: str, length: int):
-    assert isinstance(length, int), f"The length must be an integer [0-9] not {repr(length)}."
-    assert isinstance(choice, str), f"The choice must be a string not {repr(choice)}."
-    assert 8 <= length < 65556, f"The length must be any value from 8-65555" 
 
-    kitchen_sink: str = '' 
-    if choice == 'lower':
-        kitchen_sink += string.ascii_lowercase
-    elif choice == 'upper':
-        kitchen_sink += string.ascii_uppercase
-    elif choice == 'digits':
-        kitchen_sink += string.digits
-    elif choice == 'punct':
-        kitchen_sink += string.punctuation
-    elif choice == 'kitchen_sink':
-        kitchen_sink += string.ascii_letters + string.digits + string.punctuation
-    try: 
-        the_key = ''.join(secrets.choice(kitchen_sink) for i in range (length))
-    except:
-        raise RuntimeError("Please use one of the approved options") from None
-    return the_key
-   
-   
-def main():
-    message = (f"""
+def welcome_message():
+    print(f"""
         Options:
             [lower] => {string.ascii_lowercase}
             [upper] => {string.ascii_uppercase}
@@ -51,12 +18,60 @@ def main():
             [kitchen_sink] => a mixture of all of the above 
             > """)
 
-    time_to_choose = str(input(message))
-    length = int(input("Enter the length of the password you want generated [ 8 - 65555 ]: "))
-    der_wille_zur_macht = the_maker(time_to_choose, length)
-    print(der_wille_zur_macht)
+
+def password_response_handler():
+    ACCEPTABLE_CHOICES: list[str] = [
+        "lower",
+        "upper",
+        "digits",
+        "punct",
+        "kitchen_sink",
+    ]
+    kitchen_sink: str = ""
+    while True:
+        response = input("Enter an option:")
+        if response in ACCEPTABLE_CHOICES:
+            if response == 'lower':
+                kitchen_sink += string.ascii_lowercase
+            elif response == 'upper':
+                kitchen_sink += string.ascii_uppercase
+            elif response == 'digits':
+                kitchen_sink += string.digits
+            elif response == 'punct':
+                kitchen_sink += string.punctuation
+            elif response == 'kitchen_sink':
+                kitchen_sink += string.ascii_letters + string.digits + string.punctuation
+            break
+       
+        if response not in ACCEPTABLE_CHOICES:
+            print("Try again")
+            continue        
+    return kitchen_sink
+
+   
+def password_length_handler():
+    password_length: int = 0
+    while True:
+        try:
+            password_length = int(input("Enter a length for the passphrase minimum 8 maximum 65556: "))
+            if password_length < 8:
+                print("That value wasn't within range.")
+                continue
+        except:
+            print(f"That isn't a numerical value.")
+            continue
+        else:
+            break
+    return password_length
     
+    
+def the_maker():
+    welcome_message()
+    response = password_response_handler()
+    length = password_length_handler()
+    the_key = ''.join(secrets.choice(response) for i in range(length))
+    print(the_key)
+
 
 if __name__ == '__main__':
-    main()
-   
+    the_maker()
